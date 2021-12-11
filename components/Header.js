aba35fd48d2e017007241073774ed6fa57f9a27e
Sahinur Islam <infosahinur@gmail.com>
2021-12-11 15:40:39 +0600

    header somae link added and account icon update

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,7 @@ function Header() {
 
           {/* navicons */}
           <div className="space-x-4 flex items-center text-center">
-              <a href="" className="block text-center text-gray-700 hover:text-primary transition relative">
+              <div onClick={() => router.push('/wishlist')} className="block text-center text-gray-700 cursor-pointer hover:text-primary transition relative">
                   <span
                       className="absolute -right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">5</span>
                   <div className="text-2xl">
@@ -44,26 +44,26 @@ function Header() {
                     </svg>
                   </div>
                   <div className="text-xs leading-3">Wish List</div>
-              </a>
-              <a href=""
-                  className="lg:block text-center text-gray-700 hover:text-primary transition hidden relative">
+              </div>
+              <div onClick={() => router.push('/cart')}
+                  className="lg:block text-center text-gray-700 cursor-pointer hover:text-primary transition hidden relative">
                   <span
                       className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">3</span>
                   <div className="text-2xl">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
                    </svg>
                   </div>
                   <div className="text-xs leading-3">Cart</div>
-              </a>
-              <a href="" className="block text-center text-gray-700 hover:text-primary transition">
-                  <div className="text-2xl">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              </div>
+              <div onClick={() => router.push('/account')} className="block text-center cursor-pointer text-gray-700 hover:text-primary transition">
+                  <div className="text-2xl mx-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
                   </div>
                   <div className="text-xs leading-3">Account</div>
-              </a>
+              </div>
           </div>
 
       </div>
