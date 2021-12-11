51f1b53a39bc5f972fc959a0e077fd25a4b448a5
Sahinur Islam <infosahinur@gmail.com>
2021-12-11 16:08:21 +0600

    404 error page added some dami data added

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+
+function Custom404() {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            <img src="https://i.ibb.co/hL8DPZP/loader.gif" alt="Error" />
+        </div>
+    )
+}
+
+export default Custom404
