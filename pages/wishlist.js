30104697bbf01ecc59f36b5cb4978630901d387e
Sahinur Islam <infosahinur@gmail.com>
2021-12-12 02:08:38 +0600

    added search, wish list , category pages

---
+++ pages/wishlist.js
@@ -0,0 +1,18 @@
+import Copyright from "../components/Copyright"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+
+function wishlist() {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            This is Wish List page
+            <Footer />
+            <Copyright />
+        </div>
+    )
+}
+
+export default wishlist
