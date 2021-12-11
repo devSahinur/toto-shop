679ec5020828608f0cd3d779d6d9f4f07c65d287
Sahinur Islam <infosahinur@gmail.com>
2021-12-11 17:44:05 +0600

    copyriht page change

--- app/store.js
+++ app/store.js
@@ -6,3 +6,4 @@ export const store = configureStore({
     basket: basketReducer,
   },
 });
+ 
--- components/Copyright.js
+++ components/Copyright.js
@@ -1,10 +1,10 @@
 function Copyright() {
     return (
-        <div class="bg-gray-800 py-4">
-            <div class="container flex items-center justify-between">
-                <p class="text-white">© ToTo Shop - All Rights Reserved</p>
+        <div className="bg-gray-800 py-4">
+            <div className="container flex items-center justify-between">
+                <p className="text-white">© ToTo Shop - All Rights Reserved</p>
                 <div>
-                    <img src="https://i.ibb.co/L5BVHBs/methods.png" class="h-5"/>
+                    <img src="https://i.ibb.co/L5BVHBs/methods.png" className="h-5"/>
                 </div>
             </div>
         </div>
