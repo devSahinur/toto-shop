85cd69e8fac3f9fd8fd187cd48b4f0a5ff69d38e
Sahinur Islam <infosahinur@gmail.com>
2021-12-13 02:32:16 +0600

    progress bar set

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import Router from 'next/router'
 import '../styles/globals.css'
+//progressBar lib
+import ProgressBar from "@badrap/bar-of-progress";
 // custom css
 import '../styles/custom.css'
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FD3D57',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 const MyApp = ({ Component, pageProps }) => {
   return (
