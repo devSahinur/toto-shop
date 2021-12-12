49932fbf0987c2db3c983d917c0e7a9f73676ef9
Sahinur Islam <infosahinur@gmail.com>
2021-12-13 00:45:44 +0600

    middle ads added

---
+++ components/Home/MiddleAds.js
@@ -0,0 +1,9 @@
+export const MiddleAds = () => {
+    return (
+        <div class="container pb-16">
+        <a href="#">
+            <img src="https://i.ibb.co/BqVhHrz/offer.jpg" class="w-full"/>
+        </a>
+    </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import { Categories } from '../components/Home/Categories/Categories'
 import { TopNewArrival } from '../components/Home/TopNewArrival/TopNewArrival'
+import { MiddleAds } from '../components/Home/MiddleAds'
 
 export default function Home() {
   return (
@@ -23,6 +24,7 @@ export default function Home() {
       <Features />
       <Categories />
       <TopNewArrival />
+      <MiddleAds />
       </main>
 
       <Footer />
