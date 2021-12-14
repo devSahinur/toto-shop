3532e1b46f54495cc50abd6b19204073306d3c6d
Sahinur Islam <infosahinur@gmail.com>
2021-12-15 00:25:39 +0600

    404 page fix

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,9 @@ import { ArrowRightIcon, HomeIcon } from "@heroicons/react/solid";
 import Copyright from "../components/Copyright";
 import Footer from "../components/Footer";
 import Image from "next/image";
+import { useRouter } from "next/router";
 function Custom404() {
+  const router = useRouter();
   return (
     <div>
       <Header />
@@ -14,13 +16,6 @@ function Custom404() {
       {/* main-Content */}
 
       <main className="sm:max-w-[50rem] xl:max-w-[75rem] mx-auto px-5 md:px-8 py-6 space-y-6">
-        <div className="flex items-center sm:space-x-4 space-x-2 py-6">
-          <Link href="/">
-            <HomeIcon className="h-4 sm:h-5 cursor-pointer text-primary" />
-          </Link>
-          <ArrowRightIcon className="h-4 sm:h-5 cursor-pointer" />
-          <h3 className="font-light sm:text-xl">404</h3>
-        </div>
         {/* bottom containt */}
         <div className="max-w-[55rem] mx-auto px-4 py-6 pt-0 flex items-center justify-center flex-col space-y-5">
           <div className="w-full">
@@ -31,9 +26,9 @@ function Custom404() {
             <h1 className="text-xl md:text-2xl xl:text-3xl md:font-semibold pb-8">
               THE PAGE YOU'VE REQUESTED IS NOT AVAILABLE
             </h1>
-            <a className="btn uppercase">
-              <Link href="/">Back to Home</Link>
-            </a>
+            <div onClick={() => router.push("/")} className="btn cursor-pointer uppercase">
+              Back to Home
+            </div>
           </div>
         </div>
       </main>
