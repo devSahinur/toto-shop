24ebcad97ef834f62cbd83e9540ada9ea526a896
Sahinur Islam <infosahinur@gmail.com>
2021-12-14 23:04:45 +0600

    shop page fix

--- pages/shop.js
+++ pages/shop.js
@@ -6,22 +6,29 @@ import Navbar from "../components/Navbar";
 import Link from "next/link";
 import ShopSidebar from "../components/ShopGridPage/ShopSidebar/ShopSidebar";
 import ShopContent from "../components/ShopGridPage/ShopContent/ShopContent";
+import { useRouter } from "next/router";
 
 function ShopGrid() {
+  const router = useRouter();
   return (
     <div>
       <Header />
       <Navbar />
       {/* main-Content */}
+      <div className="py-4 container flex gap-3 items-center">
+        <div
+          onClick={() => router.push("/")}
+          className="text-primary cursor-pointer text-base"
+        >
+          <i className="fas fa-home"></i>
+        </div>
+        <span className="text-sm text-gray-400">
+          <i className="fas fa-chevron-right"></i>
+        </span>
+        Shop
+      </div>
 
       <main className="sm:max-w-[70rem] xl:max-w-7xl mx-auto px-5 md:px-8 py-6 space-y-6">
-        <div className="flex items-center sm:space-x-4 space-x-2 py-6">
-          <Link href="/">
-            <HomeIcon className="h-4 sm:h-5 cursor-pointer text-primary" />
-          </Link>
-          <ArrowRightIcon className="h-4 sm:h-5 cursor-pointer" />
-          <h3 className="font-light sm:text-xl">Privacy And Policy</h3>
-        </div>
 
         {/* content Main */}
 
