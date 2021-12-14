1e67fc047da0ba7c6cfb8d6931461f155706b805
Sahinur Islam <infosahinur@gmail.com>
2021-12-15 00:17:47 +0600

    home meta tag fix and terms and condition route fix

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>ToTo Shop</title>
+        <title>ToToSHOP - Online Shopping Website</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="ToToSHOP - Online Shopping Website"></meta>
+        <meta name="description" content="Bangladesh's best online shopping store with 17+ million products at resounding discounts in dhaka, ctg & All across Bangladesh with cash on delivery (COD)"></meta>
       </Head>
       <Header />
       <Navbar />
