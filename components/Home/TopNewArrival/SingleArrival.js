e1113a6ba9ef218e75277ba9d016a965f13b072f
Sahinur Islam <infosahinur@gmail.com>
2021-12-15 16:12:35 +0600

    single arrival page router fix

--- components/Home/TopNewArrival/SingleArrival.js
+++ components/Home/TopNewArrival/SingleArrival.js
@@ -83,33 +83,35 @@
 // // back home
 
 import { HeartIcon, SearchIcon } from "@heroicons/react/solid";
+import { useRouter } from "next/router";
 
 function SingleArrival({ product }) {
+  const router = useRouter();
   return (
     <div className="group rounded bg-white shadow overflow-hidden">
       <div className="relative">
         <img src={product?.image} className="w-full" />
         <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
-          <a
-            href="view"
-            className="text-white text-lg w-9 h-9 rounded-full bg-primary hover:bg-gray-800 transition flex items-center justify-center"
+          <div
+            onClick={() => router.push("/view")}
+            className="text-white text-lg w-9 h-9 cursor-pointer rounded-full bg-primary hover:bg-gray-800 transition flex items-center justify-center"
           >
             <SearchIcon className="h-5 w-5" />
-          </a>
-          <a
-            href="#"
-            className="text-white text-lg w-9 h-9 rounded-full bg-primary hover:bg-gray-800 transition flex items-center justify-center"
+          </div>
+          <div
+            onClick={() => router.push("/wishlist")}
+            className="text-white text-lg w-9 h-9 rounded-full cursor-pointer bg-primary hover:bg-gray-800 transition flex items-center justify-center"
           >
             <HeartIcon className="h-5 w-5" />
-          </a>
+          </div>
         </div>
       </div>
       <div className="pt-4 pb-3 px-4">
-        <a href="view.html">
-          <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
+        <div onClick={() => router.push("/view")}>
+          <h4 className="uppercase font-medium text-xl cursor-pointer mb-2 text-gray-800 hover:text-primary transition">
             {product?.title}
           </h4>
-        </a>
+        </div>
         <div className="flex items-baseline mb-1 space-x-2">
           <p className="text-xl text-primary font-roboto font-semibold">
             ${product.price}
