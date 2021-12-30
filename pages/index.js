54b95da389b23e07f5c94afd0b4c0be1669e4e06
Sahinur Islam <infosahinur@gmail.com>
2021-12-30 11:58:23 +0600

    ads modal commment out

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,10 @@ export default function Home() {
         <title>ToToSHOP - Online Shopping Website</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="title" content="ToToSHOP - Online Shopping Website"></meta>
-        <meta name="description" content="Bangladesh's best online shopping store with 17+ million products at resounding discounts in dhaka, ctg & All across Bangladesh with cash on delivery (COD)"></meta>
+        <meta
+          name="description"
+          content="Bangladesh's best online shopping store with 17+ million products at resounding discounts in dhaka, ctg & All across Bangladesh with cash on delivery (COD)"
+        ></meta>
       </Head>
       <Header />
       <Navbar />
