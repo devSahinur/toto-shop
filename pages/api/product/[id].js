81c31b7f88fcc82cda5f25ba8111834cc738da5a
Sahinur Islam <infosahinur@gmail.com>
2022-01-19 12:49:15 +0600

    daynamic product route done

---
+++ pages/api/product/[id].js
@@ -0,0 +1,58 @@
+import dbConnect from "./../../../lib/dbConnect";
+import Product from "./../../../models/Product";
+dbConnect();
+
+export default async (req, res) => {
+    const {
+        query: { id },
+        method
+    } = req;
+
+    switch (method) {
+        case 'GET':
+            try {
+                const product = await Product.findById(id);
+
+                if (!product) {
+                    return res.status(400).json({ success: false });
+                }
+
+                res.status(200).json({ success: true, data: product });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        case 'PUT':
+            try {
+                const product = await Product.findByIdAndUpdate(id, req.body, {
+                    new: true,
+                    runValidators: true
+                });
+
+                if (!product) {
+                    return res.status(400).json({ success: false });
+                }
+
+                res.status(200).json({ success: true, data: product });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        case 'DELETE':
+            try {
+                const deletedProduct = await Note.deleteOne({ _id: id });
+
+                if (!deletedProduct) {
+                    return res.status(400).json({ success: false })
+                }
+
+                res.status(200).json({ success: true, data: {} });
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break;
+        default:
+            res.status(400).json({ success: false })
+            break;
+    }
+}
