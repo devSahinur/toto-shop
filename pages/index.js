e5e36ef129eb3bcf277ffedf7e240af65e7c3c3f
Sahinur Islam <infosahinur@gmail.com>
2022-01-31 16:55:35 +0600

    index page Layout set

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Head from "next/head";
-import Copyright from "../components/Copyright";
-import Footer from "../components/Footer";
 import Banner from "../components/Home/Banner";
 import Features from "../components/Home/Features";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import { Categories } from "../components/Home/Categories/Categories";
 import { TopNewArrival } from "../components/Home/TopNewArrival/TopNewArrival";
 import { MiddleAds } from "../components/Home/MiddleAds";
 import { RecomendedProduct } from "../components/Home/RecomendedProduct/RecomendedProduct";
 import { useEffect, useState } from "react";
 import Modal from "../components/Home/Modal/Modal";
+import Layout from "../components/layout";
 
 export default function Home() {
   // const [showModal, setShowModal] = useState(false);
@@ -32,23 +29,18 @@ export default function Home() {
           content="Bangladesh's best online shopping store with 17+ million products at resounding discounts in dhaka, ctg & All across Bangladesh with cash on delivery (COD)"
         ></meta>
       </Head>
-      <Header />
-      <Navbar />
-
-      <main>
-        <Banner />
-        <Features />
-        <Categories />
-        <TopNewArrival />
-        <MiddleAds />
-        <RecomendedProduct />
-      </main>
-
-      {/* for show the modal */}
-      {/* {showModal && <Modal setShowModal={setShowModal} />} */}
-
-      <Footer />
-      <Copyright />
+      <Layout>
+        <main>
+          <Banner />
+          <Features />
+          <Categories />
+          <TopNewArrival />
+          <MiddleAds />
+          <RecomendedProduct />
+        </main>
+        {/* for show the modal */}
+        {/* {showModal && <Modal setShowModal={setShowModal} />} */}
+      </Layout>
     </div>
   );
 }
