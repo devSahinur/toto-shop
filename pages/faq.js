60735b87c905eb1ee84d6ea623aea6b5730d5dd4
Sahinur Islam <infosahinur@gmail.com>
2022-01-31 16:51:52 +0600

    faq page Layout set

--- pages/faq.js
+++ pages/faq.js
@@ -1,14 +1,10 @@
-import Copyright from "../components/Copyright";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import { ArrowRightIcon, HomeIcon, PlusIcon } from "@heroicons/react/solid";
-import Link from "next/link";
 import Faq from "react-faq-component";
 import ShoppingData from "../components/FaqPage/ShoppingFaqData/ShoppingFaqData";
 import OrderData from "../components/FaqPage/OrderReturnFaqData/OrderFaqData";
 import PaymentData from "../components/FaqPage/PaymentFaqData/PaymentFaqData";
 import { useRouter } from "next/router";
+import Layout from "../components/layout";
 
 function faq() {
   const router = useRouter();
@@ -20,82 +16,66 @@ function faq() {
 
   return (
     <div>
-      <Header />
-      <Navbar />
-      {/* this is the main content */}
-      <div className="py-4 container flex gap-3 items-center">
-        <div
-          onClick={() => router.push("/")}
-          className="text-primary cursor-pointer text-base"
-        >
-          <i className="fas fa-home"></i>
-        </div>
-        <span className="text-sm text-gray-400">
-          <i className="fas fa-chevron-right"></i>
-        </span>
-        FAQ
-      </div>
-      <main className="max-w-[75rem] mx-auto px-4 py-6">
-        {/* main content  top*/}
-        <div className="px-1 md:px-8 space-y-6 max-w-[550px] pt-4 pb-10">
-
-          {/* bottom Details */}
-          <h1 className="font-medium text-2xl md:text-3xl md:font-semibold">
-            F.A.Q
-          </h1>
-          <p className="text-gray-700">
-            Can’t find the answer you’re looking for? We’ve shared some of your
-            most frequently asked questions to help you out!
-          </p>
-        </div>
-
-        {/* faq wrapper */}
-        <div className="px-6 max-w-5xl space-y-10">
-          {/* shoping faq */}
-          <div>
-            <Faq
-              data={ShoppingData}
-              styles={{
-                rowTitleColor: "#FD3D57",
-                rowContentColor: "#777",
-                rowContentPaddingLeft: "10px",
-                arrowColor: "gray",
-              }}
-              config={config}
-            />
+      <Layout title={"FAQ"}>
+        <main className="max-w-[75rem] mx-auto px-4 py-6">
+          {/* main content  top*/}
+          <div className="px-1 md:px-8 space-y-6 max-w-[550px] pt-4 pb-10">
+            {/* bottom Details */}
+            <h1 className="font-medium text-2xl md:text-3xl md:font-semibold">
+              F.A.Q
+            </h1>
+            <p className="text-gray-700">
+              Can’t find the answer you’re looking for? We’ve shared some of
+              your most frequently asked questions to help you out!
+            </p>
           </div>
 
-          {/* order Faq */}
-          <div>
-            <Faq
-              data={OrderData}
-              styles={{
-                rowTitleColor: "#FD3D57",
-                rowContentColor: "#777",
-                rowContentPaddingLeft: "10px",
-                arrowColor: "gray",
-              }}
-              config={config}
-            />
-          </div>
+          {/* faq wrapper */}
+          <div className="px-6 max-w-5xl space-y-10">
+            {/* shoping faq */}
+            <div>
+              <Faq
+                data={ShoppingData}
+                styles={{
+                  rowTitleColor: "#FD3D57",
+                  rowContentColor: "#777",
+                  rowContentPaddingLeft: "10px",
+                  arrowColor: "gray",
+                }}
+                config={config}
+              />
+            </div>
 
-          {/* order Faq */}
-          <div>
-            <Faq
-              data={PaymentData}
-              styles={{
-                rowTitleColor: "#FD3D57",
-                rowContentColor: "#777",
-                rowContentPaddingLeft: "10px",
-                arrowColor: "gray",
-              }}
-              config={config}
-            />
+            {/* order Faq */}
+            <div>
+              <Faq
+                data={OrderData}
+                styles={{
+                  rowTitleColor: "#FD3D57",
+                  rowContentColor: "#777",
+                  rowContentPaddingLeft: "10px",
+                  arrowColor: "gray",
+                }}
+                config={config}
+              />
+            </div>
+
+            {/* order Faq */}
+            <div>
+              <Faq
+                data={PaymentData}
+                styles={{
+                  rowTitleColor: "#FD3D57",
+                  rowContentColor: "#777",
+                  rowContentPaddingLeft: "10px",
+                  arrowColor: "gray",
+                }}
+                config={config}
+              />
+            </div>
           </div>
-        </div>
-      </main>
-      <Footer />
-      <Copyright />
+        </main>
+      </Layout>
     </div>
   );
 }
