2f330f9a56b558cdad05269e235de22e8b470c2c
Sahinur Islam <infosahinur@gmail.com>
2022-01-31 16:43:44 +0600

    cart page Layout set

--- pages/cart.js
+++ pages/cart.js
@@ -1,31 +1,24 @@
 import CartTitleBar from "../components/CartPage/CartTitleBar";
 import OrderSummary from "../components/CartPage/OrderSummary";
 import { SingleCart } from "../components/CartPage/SingleCart";
-import Breadcrum from "../components/commonComponents/Breadcrum";
-import Copyright from "../components/Copyright";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Navbar from "../components/Navbar";
+import Layout from "../components/layout";
 
 function cart() {
   return (
     <div>
-      <Header />
-      <Navbar />
-      <Breadcrum title={"Shopping Cart"} />
-      <div className="container lg:grid grid-cols-12 gap-6 items-start pb-16 pt-4">
-        <div className="xl:col-span-9 lg:col-span-8">
-          <CartTitleBar />
-          <div className="space-y-4">
-            <SingleCart />
-            <SingleCart />
-            <SingleCart />
+      <Layout title={"Shopping Cart"}>
+        <div className="container lg:grid grid-cols-12 gap-6 items-start pb-16 pt-4">
+          <div className="xl:col-span-9 lg:col-span-8">
+            <CartTitleBar />
+            <div className="space-y-4">
+              <SingleCart />
+              <SingleCart />
+              <SingleCart />
+            </div>
           </div>
+          <OrderSummary />
         </div>
-        <OrderSummary />
-      </div>
-      <Footer />
-      <Copyright />
+      </Layout>
     </div>
   );
 }
