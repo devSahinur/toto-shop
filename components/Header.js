6d0e5145c504c665c688416a0a748c704802acf2
Sahinur Islam <infosahinur@gmail.com>
2022-02-02 09:53:07 +0600

    db to live serch fix

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 function Header() {
@@ -28,9 +27,9 @@ function Header() {
   };
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+    fetch("/api/product")
       .then((res) => res.json())
-      .then((data) => setFatchData(data))
+      .then((data) => setFatchData(data.data))
       .catch((err) => console.log(err));
   }, []);
 
@@ -109,12 +108,12 @@ function Header() {
                 {filterData?.map((item) => (
                   <li
                     key={item.id}
-                    onClick={() => router.push("/view")}
+                    onClick={() => router.push(`/product/${item._id}`)}
                     className="grid grid-cols-4 cursor-pointer bg-gray-50 hover:bg-gray-100 px-2 py-3 rounded-md"
                   >
                     <div className="w-[150px]">
                       <Image
-                        src={item.image}
+                        src={item.image[0]}
                         className="object-contain"
                         width={70}
                         height={50}
