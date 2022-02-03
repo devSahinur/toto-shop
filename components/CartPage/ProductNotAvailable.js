55cdffce32d310558949f952f167c84d07e6abff
Sahinur Islam <infosahinur@gmail.com>
2022-02-03 15:33:27 +0600

    cart produt not avaiable

---
+++ components/CartPage/ProductNotAvailable.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Copyright from "../Copyright";
+import Header from "../Header";
+import Navbar from "../Navbar";
+
+function ProductNotAvailable() {
+    const router = useRouter();
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main className="sm:max-w-[50rem] xl:max-w-[75rem] mx-auto px-5 md:px-8 py-6 space-y-6">
+        {/* bottom containt */}
+        <div className="max-w-[55rem] mx-auto px-4 py-6 pt-6 flex items-center justify-center flex-col space-y-5">
+          <div className="w-full pl-0 md:pl-20 xl:pl-20 lg:pl-20">
+            <Image
+              src="https://i.ibb.co/ncPcTGf/empty-cart.gif"
+              width={650}
+              height={350}
+            />
+          </div>
+
+          <div className="py-4 text-center font-medium">
+            <div
+              onClick={() => router.push("/")}
+              className="btn"
+            >
+              Go back and continue shopping
+            </div>
+          </div>
+        </div>
+      </main>
+      <Copyright />
+    </>
+  );
+}
+
+export default ProductNotAvailable;
--- pages/cart.js
+++ pages/cart.js
@@ -4,14 +4,16 @@ import { SingleCart } from "../components/CartPage/SingleCart";
 import Layout from "../components/layout";
 import { useSelector } from "react-redux";
 import { selectItems } from "../slices/appSlice";
+import ProductNotAvailable from "../components/CartPage/ProductNotAvailable";
 
 function cart() {
   const cartData = useSelector(selectItems);
 
   console.log(cartData[0]?.product?._id);
   return (
-    <div>
-      <Layout title={"Shopping Cart"}>
+    <>
+      <ProductNotAvailable />
+      {/* <Layout title={"Shopping Cart"}>
         <div className="container lg:grid grid-cols-12 gap-6 items-start pb-16 pt-4">
           <div className="xl:col-span-9 lg:col-span-8">
             <CartTitleBar />
@@ -27,8 +29,8 @@ function cart() {
           </div>
           <OrderSummary />
         </div>
-      </Layout>
-    </div>
+      </Layout> */}
+    </>
   );
 }
 
