706d408408b83666c3abf0a15de59dc89074187d
Sahinur Islam <infosahinur@gmail.com>
2022-02-07 18:51:25 +0600

    my product title bar fix

--- components/MyProductTitleBar.js
+++ components/MyProductTitleBar.js
@@ -1,12 +1,17 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 function MyProductTitleBar() {
+  const router = useRouter();
   return (
     <div className="bg-gray-200 hidden md:flex mb-4  p-5 py-2 justify-between">
       <p className="text-gray-600  text-center">Product Title And Price</p>
       <p className="text-gray-600  text-center "> {/* Quantity*/}</p>
       <p className=" text-gray-600  text-center">Sold</p>
-      <p className="  text-green-600 text-center">
+      <p
+        onClick={() => router.push("/sellingOrders")}
+        className="  cursor-pointer text-green-600 text-center"
+      >
         Add Product
         <i className="fas text-green-600 fa-plus-circle"></i>
       </p>
