c38afb29e62d042c46af48bab0c317b1aec3536e
Sahinur Islam <infosahinur@gmail.com>
2022-02-09 23:59:04 +0600

    data data fatech fix

--- components/Home/Categories/SingleCategories.js
+++ components/Home/Categories/SingleCategories.js
@@ -1,14 +1,20 @@
+import Link from "next/link";
+
 export const SingleCategories = ({ data }) => {
   return (
-    <div className="relative group rounded-sm overflow-hidden">
-      <img src={data?.image} className="w-full" />
-      <a
-        href="#"
-        className="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-50 flex items-center justify-center text-xl text-white 
+    <Link href={{ pathname: "/shop", query: { category: data.title } }}>
+      <a>
+        <div className="relative group rounded-sm overflow-hidden">
+          <img src={data?.image} className="w-full" />
+          <a
+            href="#"
+            className="absolute inset-0 bg-black bg-opacity-40 group-hover:bg-opacity-50 flex items-center justify-center text-xl text-white 
                     font-roboto font-medium tracking-wide transition"
-      >
-        {data?.title}
+          >
+            {data?.title}
+          </a>
+        </div>
       </a>
-    </div>
+    </Link>
   );
 };
--- pages/shop.js
+++ pages/shop.js
@@ -2,8 +2,13 @@ import ShopSidebar from "../components/ShopGridPage/ShopSidebar/ShopSidebar";
 import ShopContent from "../components/ShopGridPage/ShopContent/ShopContent";
 import { useRouter } from "next/router";
 import Layout from "../components/layout";
+import querySearch from "../lib/querySearch";
+import spliceData from "../lib/spliceData";
 
-function ShopGrid() {
+const itemsPerPage = 20 // Number of Card per page (for pagination)
+
+function ShopGrid(props) {
+  console.log(props)
   return (
     <div>
       <Layout title={"Shop"}>
@@ -21,3 +26,30 @@ function ShopGrid() {
 }
 
 export default ShopGrid;
+
+export async function getServerSideProps(context) {
+  const keyword = context.query.keyword ? context.query.keyword : ""
+  const category = context.query.category ? context.query.category : ""
+  const minprice = context.query.minprice ? context.query.minprice : ""
+  const maxprice = context.query.maxprice ? context.query.maxprice : ""
+  const sortby = context.query.sort ? context.query.sort : "Latest"
+  const data = await querySearch(keyword, category, minprice, maxprice, sortby)
+
+  const dataConvert = JSON.parse(JSON.stringify(data))
+  // Paginate session
+  const page = context.query.page ? context.query.page : "1"
+  const currentItems = spliceData(dataConvert, page, itemsPerPage)
+  const totalItems = dataConvert.length
+  
+
+  return {
+    props: {
+      keyword,
+      category,
+      currentItems,
+      sortby,
+      totalItems,
+      // totalpro
+    },
+  }
+}
