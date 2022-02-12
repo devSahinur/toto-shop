c6cb5073463965f9437933fc288dcc4fc6882460
Faysal Mridha <fmridha166@.com>
2022-02-12 23:59:04 +0600

    bug fixing

--- components/EditProduct/EditProductImage.js
+++ components/EditProduct/EditProductImage.js
@@ -8,23 +8,23 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
         <div className="space-y-6">
           <div className="grid grid-cols-2 space-x-4">
             {/* TODO: Single Image 1  */}
-            <div
-              className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative"
-              onClick={
-                (() => document.querySelector(".input_file").click(),
-                () => imageRemoveControl(`${images[0]}`))
-              }
-            >
+            <div className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative">
               {images.length >= 1 ? (
                 <>
                   <img
                     className="object-contain w-full h-full"
                     src={images[0]}
                   />
-                  <XIcon className="absolute top-0 right-0 h-5 cursor-pointer" />
+                  <XIcon
+                    onClick={() => imageRemoveControl(`${images[0]}`)}
+                    className="absolute top-0 right-0 h-5 cursor-pointer"
+                  />
                 </>
               ) : (
-                <>
+                <div
+                  className="flex items-center flex-col"
+                  onClick={() => document.querySelector(".input_file").click()}
+                >
                   <input
                     type="file"
                     onChange={handleImageUpload}
@@ -32,28 +32,28 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
                   />
                   <PhotographIcon className="h-8 cursor-pointer hover:text-primary" />
                   <h3 className="">Add Image</h3>
-                </>
+                </div>
               )}
             </div>
             {/* FIXME: 2nd one */}
             {/* TODO: Single Image 1  */}
-            <div
-              className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative"
-              onClick={
-                (() => document.querySelector(".input_file").click(),
-                () => imageRemoveControl(`${images[1]}`))
-              }
-            >
+            <div className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative">
               {images.length >= 2 ? (
                 <>
                   <img
                     className="object-contain w-full h-full"
                     src={images[1]}
                   />
-                  <XIcon className="absolute top-0 right-0 h-5 cursor-pointer" />
+                  <XIcon
+                    onClick={() => imageRemoveControl(`${images[1]}`)}
+                    className="absolute top-0 right-0 h-5 cursor-pointer"
+                  />
                 </>
               ) : (
-                <>
+                <div
+                  className="flex items-center flex-col"
+                  onClick={() => document.querySelector(".input_file").click()}
+                >
                   <input
                     type="file"
                     onChange={handleImageUpload}
@@ -61,7 +61,7 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
                   />
                   <PhotographIcon className="h-8 cursor-pointer hover:text-primary" />
                   <h3>Add Image</h3>
-                </>
+                </div>
               )}
             </div>
           </div>
@@ -69,23 +69,23 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
           {/* FIXME: another three one */}
           <div className="grid grid-cols-3 space-x-3">
             {/* TODO: Single Image 1  */}
-            <div
-              className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative"
-              onClick={
-                (() => document.querySelector(".input_file").click(),
-                () => imageRemoveControl(`${images[2]}`))
-              }
-            >
+            <div className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative">
               {images.length >= 3 ? (
                 <>
                   <img
                     className="object-contain w-full h-full"
                     src={images[2]}
                   />
-                  <XIcon className="absolute top-0 right-0 h-5 cursor-pointer" />
+                  <XIcon
+                    onClick={() => imageRemoveControl(`${images[2]}`)}
+                    className="absolute top-0 right-0 h-5 cursor-pointer"
+                  />
                 </>
               ) : (
-                <>
+                <div
+                  className="flex items-center flex-col"
+                  onClick={() => document.querySelector(".input_file").click()}
+                >
                   <input
                     type="file"
                     onChange={handleImageUpload}
@@ -93,28 +93,28 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
                   />
                   <PhotographIcon className="h-8 cursor-pointer hover:text-primary" />
                   <h3>Add Image</h3>
-                </>
+                </div>
               )}
             </div>
             {/* FIXME: 2nd one */}
             {/* TODO: Single Image 1  */}
-            <div
-              className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative"
-              onClick={
-                (() => document.querySelector(".input_file").click(),
-                () => imageRemoveControl(`${images[3]}`))
-              }
-            >
+            <div className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative">
               {images.length >= 4 ? (
                 <>
                   <img
                     className="object-contain w-full h-full"
                     src={images[3]}
                   />
-                  <XIcon className="absolute top-0 right-0 h-5 cursor-pointer" />
+                  <XIcon
+                    onClick={() => imageRemoveControl(`${images[3]}`)}
+                    className="absolute top-0 right-0 h-5 cursor-pointer"
+                  />
                 </>
               ) : (
-                <>
+                <div
+                  className="flex items-center flex-col"
+                  onClick={() => document.querySelector(".input_file").click()}
+                >
                   <input
                     type="file"
                     onChange={handleImageUpload}
@@ -122,27 +122,27 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
                   />
                   <PhotographIcon className="h-8 cursor-pointer hover:text-primary" />
                   <h3>Add Image</h3>
-                </>
+                </div>
               )}
             </div>
             {/* TODO: Single Image 1  */}
-            <div
-              className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative"
-              onClick={
-                (() => document.querySelector(".input_file").click(),
-                () => imageRemoveControl(`${images[4]}`))
-              }
-            >
+            <div className="text-center flex items-center flex-col justify-center h-[200px] rounded-md border-dashed border-2 text-gray-500 relative">
               {images.length >= 5 ? (
                 <>
                   <img
                     className="object-contain w-full h-full"
                     src={images[4]}
                   />
-                  <XIcon className="absolute top-0 right-0 h-5 cursor-pointer" />
+                  <XIcon
+                    onClick={() => imageRemoveControl(`${images[4]}`)}
+                    className="absolute top-0 right-0 h-5 cursor-pointer"
+                  />
                 </>
               ) : (
-                <>
+                <div
+                  className="flex items-center flex-col"
+                  onClick={() => document.querySelector(".input_file").click()}
+                >
                   <input
                     type="file"
                     onChange={handleImageUpload}
@@ -151,7 +151,7 @@ function EditProductImage({ handleImageUpload, images, imageRemoveControl }) {
                   />
                   <PhotographIcon className="h-8 cursor-pointer hover:text-primary" />
                   <h3>Add Image</h3>
-                </>
+                </div>
               )}
             </div>
           </div>
