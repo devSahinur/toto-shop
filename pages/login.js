5b8036af5d190f8ee7f6254f40de392ac7b761bd
Sahinur Islam <infosahinur@gmail.com>
2022-02-13 04:46:23 +0600

    login page head fix

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,26 @@
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react"
 import { Register } from "../components/Auth/Register";
 import Layout from "../components/layout";
 
 function login() {
   return (
-    <div>
+    <>
+    <Head>
+        <title>Login | ToTo SHOP</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" content="ToToSHOP - Online Shopping Website"></meta>
+        <meta
+          name="description"
+          content="Bangladesh's best online shopping store with 17+ million products at resounding discounts in dhaka, ctg & All across Bangladesh with cash on delivery (COD)"
+        ></meta>
+      </Head>
       <Layout title={"no"}>
         <main>
           <Register />
         </main>
       </Layout>
-    </div>
+    </>
   );
 }
 
