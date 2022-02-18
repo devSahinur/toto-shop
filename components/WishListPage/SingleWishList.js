fceffbe8c514207de01778d56cb3ffd40227e7e9
Sahinur Islam <infosahinur@gmail.com>
2022-02-18 23:36:38 +0600

    new

--- components/WishListPage/SingleWishList.js
+++ components/WishListPage/SingleWishList.js
@@ -1,18 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addToBasket, selectItems } from "../../slices/appSlice";
-import { removeFromWish, selectWish } from "../../slices/wishSlice";
+import { removeFromWish } from "../../slices/wishSlice";
 
 function SingleWishList({ product }) {
   const dispatch = useDispatch();
-
-  const AddtoCart = () => {
-    dispatch(
-      addToBasket({
-        product: product,
-        quantity: 1,
-      })
-    );
-  };
 
   const cartDataAll = useSelector(selectItems);
 
@@ -20,13 +11,32 @@ function SingleWishList({ product }) {
     (item) => item.product._id === product._id
   );
 
-  console.log();
+  const image = Array.isArray(product?.image)
+    ? product.image[0]
+    : product.image;
+
+  const singleProduct = product;
+
+  const AddToCart = () => {
+    dispatch(
+      addToBasket({
+        product: {
+          image,
+          availability: singleProduct.availability,
+          _id: singleProduct._id,
+          totalQuantity: singleProduct.totalQuantity,
+          title: singleProduct.title,
+          shortDescription: singleProduct.shortDescription,
+          price: singleProduct.price,
+        },
+        quantity: 1,
+      })
+    );
+  };
 
   return (
     <>
-      {/* <!-- single wishlist --> */}
       <div className="flex items-center md:justify-between gap-4 md:gap-6 p-4 border border-gray-200 rounded flex-wrap md:flex-nowrap">
-        {/* <!-- cart image --> */}
         <div className="w-28 flex-shrink-0">
           <img
             src={
@@ -35,8 +45,6 @@ function SingleWishList({ product }) {
             className="w-full"
           />
         </div>
-        {/* <!-- cart image end --> */}
-        {/* <!-- cart content --> */}
         <div className="md:w-1/3 w-full">
           <h2 className="text-gray-800 mb-1 xl:text-base text-base font-medium uppercase">
             {product.title}
@@ -50,7 +58,6 @@ function SingleWishList({ product }) {
             )}
           </p>
         </div>
-        {/* <!-- cart content end --> */}
         <div className="">
           <p className="text-primary text-lg font-semibold">${product.price}</p>
         </div>
@@ -67,7 +74,7 @@ function SingleWishList({ product }) {
               <a
                 href="#"
                 className="ml-auto md:ml-0 block px-6 py-2 text-center text-sm text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium"
-                onClick={AddtoCart}
+                onClick={AddToCart}
               >
                 Add to cart
               </a>
@@ -89,7 +96,6 @@ function SingleWishList({ product }) {
           <i className="fas fa-trash"></i>
         </div>
       </div>
-      {/* <!-- single wishlist end --> */}
     </>
   );
 }
