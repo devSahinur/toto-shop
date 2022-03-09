fe2add0f824352d63f13114a4217aa2b63aa0a26
Faysal Mridha <fmridha166@.com>
2022-03-09 12:42:21 +0600

    remove unnessery code

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -1,13 +1,30 @@
-import React from "react";
-import AdminLayout from "../../components/Admin/AdminLayout";
+import React, { useState } from "react";
 import UserFeed from "../../components/Admin/Feed/Users/UserFeed";
+import AdminHeader from "../../components/Admin/Header/AdminHeader";
+import AdminSidebar from "../../components/Admin/SidedeBar/AdminSidebar";
 
 function users() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div>
-      <AdminLayout>
-        <UserFeed />
-      </AdminLayout>
+      <div>
+        {/* Top Header */}
+        <AdminHeader
+          showSidebar={showSidebar}
+          setShowSidebar={setShowSidebar}
+        />
+        {/* Admin COntent */}
+        <div className="flex relative">
+          {/* Admin Sidebar  */}
+          <AdminSidebar
+            showSidebar={showSidebar}
+            setShowSidebar={setShowSidebar}
+          />
+          <div className="flex-grow p-6 bg-[#F1F5F9] min-h-[90vh]">
+            <UserFeed />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
